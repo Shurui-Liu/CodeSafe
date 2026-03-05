f88d42db36a52a134f5099736e8e742bc771e2ad
Shurui-Liu <shurui.liu.710@gmail.com>
2026-03-04 22:33:47 -0800

    added terraform config

---
+++ infrastructure/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "project_name" {
+  description = "Used as a prefix for all named resources"
+  type        = string
+  default     = "codesafe"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet (EC2 lives here)"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet (DynamoDB endpoints, future use)"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "az" {
+  description = "Availability zone to use"
+  type        = string
+  default     = "us-west-2a"
+}
